fe1a1792cd4aba6b95ddb2a52ba19dfa0f5a3d12
Dennis Suratna <dennis@pathdefender.com>
2016-02-02 11:43:12 -0800

    Adding css

--- js/mcafeesecure.js
+++ js/mcafeesecure.js
@@ -1,26 +1,18 @@
 jQuery(function(){
     var $activationSection = jQuery("#mcafeesecure-activation");
+    var $dashboardSection = jQuery("#mcafeesecure-dashboard");
     var $data = jQuery("#mcafeesecure-data");
 
     var host = $data.attr('data-host');
+    console.log(host);
+    if(!host){ host = '';}
+
     var email = $data.attr('data-email');
+    console.log(host);
+    if(!email){ email = '';}
+
     var endpointUrl = 'https://staging02.pathdefender.com';
     var apiUrl = endpointUrl + '/rpc/ajax?do=lookup-site-status&jsoncallback=?&rand='+new Date().getTime()+'&host=' + encodeURIComponent(host)
-
-    jQuery.getJSON(apiUrl,function(data) {
-        console.log("lookup-site-status");
-        console.log(data);
-
-        var status = data['status'];
-
-        if(status === 'none'){
-            $activationSection.show();
-        }else{
-            var pro = data['isPro'];
-
-        }
-
-    });
 
     jQuery("#activate-now").click(function(){
         var left = window.innerWidth / 2 - 250;
@@ -28,4 +20,22 @@ jQuery(function(){
         var signupUrl = endpointUrl + "/app/wordpress2/signup?host=" + encodeURIComponent(host) + "&email=" + encodeURIComponent(email)
         var signupWindow = window.open(signupUrl, "_blank", "width=500 height=600 left=" + left + " top=" + top);
     });
+
+    function refresh(){
+        jQuery.getJSON(apiUrl,function(data) {
+            console.log("lookup-site-status");
+            console.log(data);
+
+            var status = data['status'];
+
+            if(status === 'none'){
+                $activationSection.show();
+            }else{
+                var pro = data['isPro'];
+                $dashboardSection.show();
+            }
+        });
+    }
+
+    var refreshInterval = setInterval(refresh, 1000);
 });
--- lib/settings_page.php
+++ lib/settings_page.php
@@ -1,15 +1,24 @@
 <?php
-$assets = WP_PLUGIN_URL . '/mcafee-secure/js/';
+$jsFolder = WP_PLUGIN_URL . '/mcafee-secure/js/';
+$cssFolder = WP_PLUGIN_URL . '/mcafee-secure/css/';
 $email = get_option( 'admin_email' );;
 $partner = 'wp-generic';
 $affiliate = '221269';
 $arrHost = parse_url(home_url('', $scheme = 'http'));
 $host = $arrHost['host'];
+$endpointUrl = 'https://staging02.pathdefender.com';
 ?>
-<script src="<?php echo $assets; ?>jquery.min.js"></script>
-<script src="<?php echo $assets; ?>mcafeesecure.js"></script>
-<div class="wrap" id="mcafeesecure-data">
-    <div id="mcafeesecure-data" data-host="<? echo $host; ?>" data-email="<? echo $email; ?>"></div>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="<?php echo $jsFolder; ?>jquery.min.js"></script>
+<script src="<?php echo $jsFolder; ?>mcafeesecure.js"></script>
+
+<link rel="stylesheet" href="<?php echo $cssFolder; ?>common.css" type="text/css">
+<link rel="stylesheet" href="<?php echo $cssFolder; ?>user.css" type="text/css">
+<link rel="stylesheet" href="<?php echo $cssFolder; ?>settings.css" type="text/css">
+<link rel="stylesheet" href="<?php echo $cssFolder; ?>override.css" type="text/css">
+
+<div class="wrap" id="mcafeesecure-container">
+    <div id="mcafeesecure-data" data-host="<?php echo $host; ?>" data-email="<?php echo $email; ?>"></div>
 
     <div id="mcafeesecure-activation" style="display: none;">
         <h1>McAfee SECURE Activation</h1>
@@ -18,8 +27,71 @@ $host = $arrHost['host'];
     </div>
 
     <div id="mcafeesecure-dashboard" style="display: none;">
-        <h1>McAfee SECURE Dashboard</h1>
+        <h1>McAfee SECURE</h1>
         <br/>
-        <input type="submit" name="submit" class="button button-primary" value="Activate Now" id="activate-now"></p>
+
+        <div class="wrapper">
+            <div id="content">
+                <div class="row row-toggle" data-toggle-key="mfes-security">
+                    <i class="fa fa-angle-down"></i>
+                    <div class="row-title"><i class="fa fa-check"></i><b>Security</b></div>
+                    All tests passed!
+                </div>
+
+                <div class="row row-toggle" href-linked="1">
+                    <a href="javascript:void(0);">Go Unlimited</a>
+                    <div class="row-title"><i class="fa fa-check"></i><b>Certification Trustmark</b></div>
+                    All Good
+                </div>
+
+                <div class="row lite" href-linked="1">
+                    <a href="javascript:void(0);">Learn More</a>
+                    <div class="row-title"><i class="fa fa-times"></i><b>Search Highlighting</b></div>
+
+                    <div class="pill pill-grey">Pro Feature</div> Enable highlighting in Google, Yahoo, and Bing search results.
+                </div>
+
+                <div class="row lite" href-linked="1">
+                    <a href="javascript:void(0);">Learn More</a>
+                    <div class="row-title"><i class="fa fa-times"></i><b>Engagement Trustmark</b></div>
+
+                    <div class="pill pill-grey">Pro Feature</div> Enable the engagement trustmark to boost visitor confidence.
+                </div>
+
+            </div>    
+        </div>
+        
+        <br/>
+
+        <h1>TrustedSite</h1>
+        <br/>
+
+        <div class="wrapper">
+            <div id="content">
+                <div class="row" href="<?php echo $endpointUrl; ?>/user/site/ratings?siteId=2366096" href-linked="1">
+                    <a href="javascript:void(0);">View Details</a>
+                    <div class="row-title"><i class="fa fa-check"></i><b>Site Reviews</b></div> 
+                    <div class="dot-yellow"></div> No Reviews
+                </div>
+
+                <div class="row" href="<?php echo $endpointUrl; ?>/user/site/sitemap?siteId=982208" href-linked="1">
+                    <a href="javascript:void(0);">View Details</a>
+                    <div class="row-title"><i class="fa fa-check"></i><b>Sitemap</b></div>
+                    Created <b>3 days ago</b>.  Contains <b>59</b> pages.
+                </div>
+
+                <div class="row" href="<?php echo $endpointUrl; ?>/user/site/diagnostics?siteId=982208" href-linked="1">
+                    <a href="javascript:void(0);">View Details</a>
+                    <div class="row-title"><i class="fa fa-times"></i><b>Diagnostics</b></div>
+                    3 issues found.
+                </div>
+
+                <div class="row" href="<?php echo $endpointUrl; ?>/user/site/directory?siteId=982208" href-linked="1">
+                    <a href="javascript:void(0);">Edit Profile</a>
+                    <div class="row-title"><i class="fa fa-check"></i><b>Website Profile</b></div>
+                    All good!
+                </div>
+            </div>    
+        </div>
     </div>
 </div>
